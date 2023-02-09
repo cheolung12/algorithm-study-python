fc9b1033a31010e775373e77150a2ec1cc5363af
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-02-09 18:40:07 +0900

    23.02.09

---
+++ boj/9095.py
@@ -0,0 +1,8 @@
+nTestCases = int(input())
+
+for i in range(nTestCases):
+    n = int(input())
+    dp = [0, 1, 2, 4]
+    for i in range(4, n+1):
+        dp.append(dp[i-1]+dp[i-2]+dp[i-3])
+    print(dp[n])
